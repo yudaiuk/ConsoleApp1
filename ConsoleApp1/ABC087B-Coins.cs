87dafacb73195fa48b47ffce23a82ef0eba12836
yudaiuk <yuudaiufo@icloud.com>
2024-02-18 20:03:05 +0900

    ABC087B - Coins

---
+++ ConsoleApp1/ABC087B-Coins.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ABC087B_Coins
+    {
+        public void Execlusion()
+        {
+            int fiveHundred = int.Parse(Console.ReadLine());
+            int oneHundred = int.Parse(Console.ReadLine());
+            int fifty = int.Parse(Console.ReadLine());
+            int num = int.Parse(Console.ReadLine());
+            int resultCnt = 0;
+
+            for(int a = fiveHundred; a >= 0; a--)
+            {
+                for(int b = oneHundred; b >= 0; b--)
+                {
+                    for(int c = fifty; c >= 0; c--)
+                    {
+                        if((500 * a + 100 * b + 50 * c) == num)
+                        {
+                            resultCnt++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(resultCnt);
+            
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,12 +5,13 @@ class Program
     static void Main(string[] args)
     {
         //var product = new ABC086A_Product();
-        //product.Execlusion();
 
         //var product = new ABC081A_PlacingMarbles();
-        //product.Execlusion();
 
-        var product = new ABC081B_ShiftOnly();
+        //var product = new ABC081B_ShiftOnly();
+
+        var product = new ABC087B_Coins();
+
         product.Execlusion();
     }
 }
