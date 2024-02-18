54fd3a51357ff665c594b9161a833dab571f490f
yudaiuk <yuudaiufo@icloud.com>
2024-02-18 16:06:18 +0900

    ABC081A - Placing Marbles

---
+++ ConsoleApp1/ABC081A-PlacingMarbles.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class ABC081A_PlacingMarbles
+    {
+        public void Execlusion()
+        {
+            string input = Console.ReadLine();
+
+            int resultCnt = input.Count(x => x == '1');
+
+            Console.WriteLine(resultCnt);
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        var product = new ABC086A_Product();
+        //var product = new ABC086A_Product();
+        //product.Execlusion();
+
+        var product = new ABC081A_PlacingMarbles();
         product.Execlusion();
     }
 }
